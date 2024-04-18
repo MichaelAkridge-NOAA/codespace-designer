2ab8b3eccbe6c8ca2853cd27440390123c7e2c80
Michael Akridge - NOAA <65312823+MichaelAkridge-NOAA@users.noreply.github.com>
2024-04-18 13:13:36 -1000

    Update loadGenerateDevCon.js

--- assets/js/loadGenerateDevCon.js
+++ assets/js/loadGenerateDevCon.js
@@ -5,46 +5,45 @@ function generateDevContainerJSON() {
     const extensions = Array.from(document.getElementById('extensionSelector').selectedOptions).map(opt => opt.value);
     const rawPorts = document.getElementById('ports').value;
     const ports = rawPorts.split(',').map(port => port.trim()).filter(port => port !== ''); // Filters out empty entries
-    const updateContentCommand = document.getElementById('updateContentCommand').value;
-    const postCreateCommand = document.getElementById('postCreateCommand').value;
-    const postStartCommand = document.getElementById('postStartCommand').value;
-
-    // Prepend the text to the selected image value
     const baseImageURL = "ghcr.io/nmfs-opensci/";
     const selectedImage = document.getElementById('packageSelector').value;
     const image = baseImageURL + selectedImage;
 
-    const devContainerConfig = {
+    let devContainerConfig = {
         "name": name,
         "workspaceFolder": workspaceFolder,
         "image": image,
         "hostRequirements": {
-            "cpus": parseInt(cpus)
+            "cpus": cpus
         },
         "customizations": {
             "vscode": {
                 "extensions": extensions
             }
-        },
-        "updateContentCommand": updateContentCommand,
-        "postCreateCommand": postCreateCommand,
-        "postStartCommand": postStartCommand
+        }
     };
 
-    // Add ports only if there are valid entries
+    // Optional commands
+    ["updateContentCommand", "postCreateCommand", "postStartCommand"].forEach(cmd => {
+        const commandValue = document.getElementById(cmd).value.trim();
+        if (commandValue) {
+            devContainerConfig[cmd] = commandValue;
+        }
+    });
+
     if (ports.length > 0) {
         devContainerConfig.forwardPorts = ports.map(Number);
         devContainerConfig.portsAttributes = {};
         ports.forEach(port => {
             devContainerConfig.portsAttributes[port] = {
-                "label": image,
+                "label": selectedImage,
                 "onAutoForward": "openPreview"
             };
         });
     }
 
-    // Output the JSON configuration in a formatted way
-    globalJSON = JSON.stringify(devContainerConfig, null, 2); // Store JSON in global variable for download
+    globalJSON = JSON.stringify(devContainerConfig, null, 2);
     document.getElementById('output').textContent = globalJSON;
-    document.getElementById('downloadBtn').style.display = 'inline-block';  // Show the download button
+    document.getElementById('downloadBtn').style.display = 'inline-block';
 }
+
